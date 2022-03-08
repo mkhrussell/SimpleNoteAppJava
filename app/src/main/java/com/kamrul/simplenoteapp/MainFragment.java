b9180641e26fff26c22fe0151bed45fcc819d90b
Kamrul Hasan <mkhrussell@gmail.com>
2022-03-07 22:09:32 -0500

    Added recyclerview

--- app/src/main/java/com/kamrul/simplenoteapp/MainFragment.java
+++ app/src/main/java/com/kamrul/simplenoteapp/MainFragment.java
@@ -9,17 +9,26 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.kamrul.simplenoteapp.databinding.MainFragmentBinding;
 
 public class MainFragment extends Fragment {
 
     private MainViewModel mViewModel;
+    private MainFragmentBinding binding;
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = MainFragmentBinding.inflate(inflater, container, false);
         mViewModel = new ViewModelProvider(this).get(MainViewModel.class);
 
-        return inflater.inflate(R.layout.main_fragment, container, false);
+        binding.recyclerView.setHasFixedSize(true);
+        DividerItemDecoration divider = new DividerItemDecoration(getContext(), new LinearLayoutManager(getContext()).getOrientation());
+        binding.recyclerView.addItemDecoration(divider);
+
+        return binding.getRoot();
     }
 
 }
