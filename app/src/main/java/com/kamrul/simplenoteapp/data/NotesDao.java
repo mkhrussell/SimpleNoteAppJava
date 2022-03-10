d0367d13f3c5706849b6b54fd750eb5cf082756c
Kamrul Hasan <mkhrussell@gmail.com>
2022-03-10 01:58:22 -0500

    Added NotesDao

---
+++ app/src/main/java/com/kamrul/simplenoteapp/data/NotesDao.java
@@ -0,0 +1,25 @@
+package com.kamrul.simplenoteapp.data;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface NotesDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertNote(NoteEntity note);
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insertNotes(List<NoteEntity> notes);
+
+    @Query("SELECT * FROM notes ORDER BY date ASC")
+    LiveData<List<NoteEntity>> getAll();
+
+    @Query("SELECT * FROM notes WHERE id = :id")
+    NoteEntity getNoteById(int id);
+}
